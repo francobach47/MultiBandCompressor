47f1eca612f4b980985dc1d52b9a8c00c89f2ce9
francobach47 <francobach47@gmail.com>
2024-02-27 11:42:25 -0300

    feat: global control graphics

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -8,15 +8,32 @@ Placeholder::Placeholder()
 }
 
 //==============================================================================
+void GlobalControls::paint(juce::Graphics& g)
+{
+    using namespace juce;
+    auto bounds = getLocalBounds();
+    g.setColour(Colours::blueviolet);
+    g.fillAll();
+
+    auto localBounds = bounds;
+
+    bounds.reduce(3, 3);
+    g.setColour(Colours::black);
+    g.fillRoundedRectangle(bounds.toFloat(), 3);
+
+    g.drawRect(localBounds);
+}
+
+//==============================================================================
 MultiBandCompressorAudioProcessorEditor::MultiBandCompressorAudioProcessorEditor (MultiBandCompressorAudioProcessor& p)
     : AudioProcessorEditor (&p), audioProcessor (p)
 {
     // Make sure that before the constructor has finished, you've set the
     // editor's size to whatever you need it to be.
-    addAndMakeVisible(controlBar);
-    addAndMakeVisible(analyzer);
+    //addAndMakeVisible(controlBar);
+    //addAndMakeVisible(analyzer);
     addAndMakeVisible(globalControls);
-    addAndMakeVisible(bandControls);
+    //addAndMakeVisible(bandControls);
     
     setSize (600, 500);
 }
@@ -29,12 +46,13 @@ MultiBandCompressorAudioProcessorEditor::~MultiBandCompressorAudioProcessorEdito
 void MultiBandCompressorAudioProcessorEditor::paint (juce::Graphics& g)
 {
     // (Our component is opaque, so we must completely fill the background with a solid colour)
-    g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
+    //g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
 
-    g.setColour (juce::Colours::white);
-    g.setFont (15.0f);
-    g.drawFittedText ("Hello World!", getLocalBounds(), juce::Justification::centred, 1);
-}
+    //g.setColour (juce::Colours::white);
+    //g.setFont (15.0f);
+    //g.drawFittedText ("Hello World!", getLocalBounds(), juce::Justification::centred, 1);
+    g.fillAll(juce::Colours::black);
+}   
 
 void MultiBandCompressorAudioProcessorEditor::resized()
 {
@@ -45,10 +63,9 @@ void MultiBandCompressorAudioProcessorEditor::resized()
 
     controlBar.setBounds(bounds.removeFromTop(32));
 
+    bandControls.setBounds(bounds.removeFromBottom(135));
+    
     analyzer.setBounds(bounds.removeFromTop(225));
     
     globalControls.setBounds(bounds);
-
-    bandControls.setBounds(bounds.removeFromBottom(135));
-
 }
